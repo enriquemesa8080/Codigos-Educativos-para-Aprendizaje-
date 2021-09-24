482488542f6891501cf89392f256c4fa684048e0
Enrique Mesa <33262138+enriquemesa8080@users.noreply.github.com>
2021-09-24 14:33:28 -0400

    Create swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+	int a = 20;
+	int b = 33;
+	int temp;
+
+	//Rotación de Variables 
+	//Y como resultado un intercambio
+	temp = x;
+	x = y;
+	y = temp;
+
+	return 0;
+}
